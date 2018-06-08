d3c8ad38f02d04a31ac032be3093507990a430ea
mimogollon1 <31084088+mimogollon1@users.noreply.github.com>
2018-06-08 12:16:10 -0500

    Menu

--- mundo/mundo/mundo/App.xaml.cs
+++ mundo/mundo/mundo/App.xaml.cs
@@ -9,26 +9,35 @@ namespace mundo
     using Views;
     public partial class App : Application
 	{
-		public App ()
-		{
-			InitializeComponent();
+        #region Peoperties
+        public static NavigationPage Navigator { get; internal set; }
+        #endregion
 
-            this.MainPage = new NavigationPage(new LoginPage());
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            //this.MainPage = new NavigationPage(new LoginPage());
+            this.MainPage = new MasterPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
         }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
 }
---
+++ mundo/mundo/mundo/Views/MasterPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace mundo.Views
+{
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterPage : MasterDetailPage
+	{
+		public MasterPage ()
+		{
+			InitializeComponent ();
+            App.Navigator = Navigator;
+
+        }
+	}
+}
